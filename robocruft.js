fb8c0c560f6a95e52ae9b03d9b31e086eb6c9ca0
Nathan Sherburn <nathansherburn@gmail.com>
2019-07-07 13:37:55 +1000

    Version 0.1

--- robocruft.js
+++ robocruft.js
@@ -1,1 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
+const DELIMITER = "\n";
+const FIELDS = ["email"];
+
+let input = fs.readFileSync("input", { encoding: "utf-8" });
+
+let processed = input
+  .split("\n")
+  // Remove empty lines and those that start with '/', '{' or '}'
+  .filter(line => line !== "" && !line.match(/^\/|{|}/))
+  // TODO Only keep lines with fields in FIELDS
+  // .filter(line => FIELDS.map(field => new RegExp('^s+"' + field)))
+  // Remove the field name
+  .map(line => line.replace(/^\s+"\w+" : "/, ""))
+  // Remove trailing quotation marks
+  .map(line => line.replace(/"$/, ""))
+  .join(DELIMITER);
+
+
+console.log("Finished! Check 'output' for results.\n\n")
+fs.writeFileSync("output", processed);
